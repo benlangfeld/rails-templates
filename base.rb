ddb3a7a4cd744e8bc1f0c3f867e85dcc6e968568
Ben Langfeld <ben@langfeld.co.uk>
2010-04-30 12:52:24 +0100

    Updated rspec tu use gems

--- base.rb
+++ base.rb
@@ -1,6 +1,7 @@
 if yes?("Do you want to use RSpec for testing?")
-  plugin "rspec", :git => "git://github.com/dchelimsky/rspec.git"
-  plugin "rspec-rails", :git => "git://github.com/dchelimsky/rspec-rails.git"
+  run "echo 'gem \"rspec-rails\", \">= 2.0.0.beta.8\"' >> Gemfile"
+  run "echo 'gem \"rspec\", \">= 2.0.0.beta.8\"' >> Gemfile"
+  run "bundle install"
   generate :rspec
 end
 
