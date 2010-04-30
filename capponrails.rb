a29b624990ca713058dacbe2cbb23a7e7678dd95
Ben Langfeld <ben@langfeld.co.uk>
2010-04-30 13:10:16 +0100

    Added Cappuccino generator

---
+++ capponrails.rb
@@ -0,0 +1,20 @@
+load_template "http://github.com/benlangfeld/rails-templates/raw/master/base.rb"
+
+arguments = ""
+if yes?("Do you want to use a CIB based app?")
+  arguments = "-t NibApplication"
+else
+  arguments = "-t Application"
+end
+
+appName = ask("What's the name of the app?")
+
+cappCommand = "capp gen" + appName + " " + arguments
+run cappCommand
+run "mv " + appName + " Cappuccino"
+
+file ".gitignore", <<-END
+Cappuccino/Build/*
+END
+
+git :add => "Cappuccino", :commit => "-m 'Added Cappuccino'"
