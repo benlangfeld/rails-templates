b5f054293bb6784af8825906a0f99da029fb240b
Ben Langfeld <ben@langfeld.co.uk>
2010-06-18 15:01:10 +0100

    Use maybe_update_file for authlogic user model and skip other files for testing

--- authlogic.rb
+++ authlogic.rb
@@ -5,11 +5,12 @@ generate('nifty:scaffold', "user username:string email:string password:string ne
 generate('session', "user_session")
 generate('nifty:scaffold', "user_session --skip-model username:string password:string new destroy")
 
-run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/application_controller.rb > app/controllers/application_controller.rb"
-run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/user_sessions_controller.rb > app/controllers/user_sessions_controller.rb"
-run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/users_controller.rb > app/controllers/users_controller.rb"
-run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/user.rb > app/models/user.rb"
-run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/user_session.rb > app/models/user_session.rb"
+#run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/application_controller.rb > app/controllers/application_controller.rb"
+#run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/user_sessions_controller.rb > app/controllers/user_sessions_controller.rb"
+#run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/users_controller.rb > app/controllers/users_controller.rb"
+#run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/user_session.rb > app/models/user_session.rb"
+
+maybe_update_file :file => "app/models/user.rb", :action => " make User model act as authentication source", :unless_present => /authentic/, :after => "class User < ActiveRecord::Base", :content => "  acts_as_authentic"
 
 route 'match \'login\' => \'user_sessions#new\''
 route 'match \'logout\' => \'user_sessions#destroy\''
