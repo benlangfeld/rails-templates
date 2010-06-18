43103d0ec9f04c1645949468e274356decb592b8
Ben Langfeld <ben@langfeld.co.uk>
2010-06-18 12:15:06 +0100

    Try new generate() syntax

--- authlogic.rb
+++ authlogic.rb
@@ -1,9 +1,9 @@
 gem 'authlogic', :git => "git://github.com/odorcicd/authlogic.git", :branch => "rails3"
 run "bundle install"
 
-generate(:nifty:scaffold, "user username:string email:string password:string new edit")
-generate(:session, "user_session")
-generate(:nifty:scaffold, "user_session --skip-model username:string password:string new destroy")
+generate('nifty:scaffold', "user username:string email:string password:string new edit")
+generate('session', "user_session")
+generate('nifty:scaffold', "user_session --skip-model username:string password:string new destroy")
 
 run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/application_controller.rb > app/controllers/application_controller.rb"
 run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/user_sessions_controller.rb > app/controllers/user_sessions_controller.rb"
