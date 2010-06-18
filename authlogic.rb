de30d8c171b6eb77196b690e6d5bc526d9e4a65d
Ben Langfeld <ben@langfeld.co.uk>
2010-06-18 15:16:04 +0100

    Updates to authlogic script

--- authlogic.rb
+++ authlogic.rb
@@ -5,12 +5,33 @@ generate('nifty:scaffold', "user username:string email:string password:string ne
 generate('session', "user_session")
 generate('nifty:scaffold', "user_session --skip-model username:string password:string new destroy")
 
-#run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/application_controller.rb > app/controllers/application_controller.rb"
 #run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/user_sessions_controller.rb > app/controllers/user_sessions_controller.rb"
 #run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/users_controller.rb > app/controllers/users_controller.rb"
-#run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/user_session.rb > app/models/user_session.rb"
 
-maybe_update_file :file => "app/models/user.rb", :action => " make User model act as authentication source", :unless_present => /authentic/, :after => "class User < ActiveRecord::Base", :content => "  acts_as_authentic"
+run "curl -L http://github.com/benlangfeld/rails-templates/raw/master/resources/authlogic/user_session.rb > app/models/user_session.rb"
+
+maybe_update_file :file => "app/models/user.rb", :action => "make User model act as authentication source", :unless_present => /authentic/,
+                  :after => "class User < ActiveRecord::Base", :content => "  acts_as_authentic"
+                  
+maybe_update_file :file => "app/controllers/application_controller.rb", :action => "update application controller", 
+                  :unless_present => /authentic/, :after => "class User < ActiveRecord::Base", :content => (<<-CODE).gsub(/\A +| +\Z/, '')
+                  
+                  helper :all
+                  helper_method :current_user
+
+                  private
+
+                  def current_user_session
+                    return @current_user_session if defined?(@current_user_session)
+                    @current_user_session = UserSession.find
+                  end
+
+                  def current_user
+                    return @current_user if defined?(@current_user)
+                    @current_user = current_user_session && current_user_session.record
+                  end
+                  
+                  CODE
 
 route 'match \'login\' => \'user_sessions#new\''
 route 'match \'logout\' => \'user_sessions#destroy\''
