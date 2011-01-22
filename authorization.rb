94d0544f61732a3cfdd72567da6a422a87b5d630
Ben Langfeld <ben@langfeld.me>
2011-01-23 00:18:46 +0100

    Cream and dynamic attr_accessible stuff

--- authorization.rb
+++ authorization.rb
@@ -1,7 +1,11 @@
 if y?("Use Cream (Devise, CanCan & Roles Generic) for authentication and authorization?")
-
+  gem "cream"
+  run "bundle install"
+  generate "cream:full_config", "--strategy #{ask "What role strategy should we use?"}", "--roles #{ask "What roles should we use (separate by commas)?"}"
+  git :add => ".", :commit => "-m 'Use Cream (Devise, CanCan & Roles Generic) for authentication and authorization'"
 end
 
 if y?("Automatically protect model attributes?")
-
+  run "curl -s -L #{@templates_path}/resources/accessible_attributes.rb > config/initializers/accessible_attributes.rb"
+  git :add => ".", :commit => "-m 'Automatically protect model attributes'"
 end
