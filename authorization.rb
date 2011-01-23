0e63839f699df57bb8dbb0e6c3aada1b07f7bc8b
Ben Langfeld <ben@langfeld.me>
2011-01-23 15:55:01 +0100

    Better cream defaults

--- authorization.rb
+++ authorization.rb
@@ -1,7 +1,16 @@
 if y?("Use Cream (Devise, CanCan & Roles Generic) for authentication and authorization?")
   gem "cream"
   run "bundle install"
-  generate "cream:full_config", "--strategy #{ask "What role strategy should we use?"}", "--roles #{ask "What roles should we use (separate by spaces)?"}"
+  options = []
+
+  strategy = ask "What role strategy should we use? (default is admin_flag)"
+  strategy = "admin_flag" if strategy.empty?
+  options << "--strategy #{strategy}"
+
+  roles = ask "What roles should we use (separate by spaces)? (defaults :guest and :admin)"
+  options << "--roles #{roles}" if roles.present?
+
+  generate "cream:full_config", options
   git :add => ".", :commit => "-m 'Use Cream (Devise, CanCan & Roles Generic) for authentication and authorization'"
 end
 
