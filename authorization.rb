a2d8f36885d543ff1c21e74f1be33c8382407a97
Ben Langfeld <ben@langfeld.me>
2011-01-23 14:32:51 +0100

    Typo

--- authorization.rb
+++ authorization.rb
@@ -1,7 +1,7 @@
 if y?("Use Cream (Devise, CanCan & Roles Generic) for authentication and authorization?")
   gem "cream"
   run "bundle install"
-  generate "cream:full_config", "--strategy #{ask "What role strategy should we use?"}", "--roles #{ask "What roles should we use (separate by commas)?"}"
+  generate "cream:full_config", "--strategy #{ask "What role strategy should we use?"}", "--roles #{ask "What roles should we use (separate by spaces)?"}"
   git :add => ".", :commit => "-m 'Use Cream (Devise, CanCan & Roles Generic) for authentication and authorization'"
 end
 
