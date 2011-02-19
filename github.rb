80c9f3eb852d0888d1830f890e9c9f86c6f207da
Ben Langfeld <ben@langfeld.me>
2011-02-19 11:44:55 +0000

    Tidy up github stuff

--- github.rb
+++ github.rb
@@ -1,6 +1,4 @@
 github_username = ask "What's your github username?"
 github_repo_name = ask "What's the github repo name?"
 
-github_repo_url = "git@github.com:#{github_username}/#{github_repo_name}.git"
-github_remote_add_command = "add github #{github_repo_url}"
-git :remote => github_remote_add_command, :push => "github master"
+git :remote => "add github git@github.com:#{github_username}/#{github_repo_name}.git", :push => "github master"
