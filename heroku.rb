2dba47e147da04c8f47835e7fb30410f685c9825
Ben Langfeld <ben@langfeld.me>
2011-03-06 13:10:06 +0000

    Don't auto-deploy to Heroku

--- heroku.rb
+++ heroku.rb
@@ -3,7 +3,7 @@ gem 'heroku_san', :group => :development
 bundle
 
 run 'rake heroku:create_config'
-run 'rake all heroku:create heroku:remotes heroku:rack_env deploy'
+run 'rake all heroku:create heroku:remotes heroku:rack_env'
 
 puts "You'll probably need to migrate your Heroku stack to bamboo-mri-1.9.2"
 
